22f380f51b135e2358f464e77554e186f5c452f3
Durgadevi-321 <durgakonagantla272@gmail.com>
2025-06-02 16:19:07 +0530

    Add files via upload

---
+++ calendar.java
@@ -0,0 +1,38 @@
+package project10;
+import java.util.Scanner;
+public class calendar {
+	 public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.println("Enter a weekday : ");
+	        String day = scanner.nextLine().trim().toLowerCase();
+
+	        switch (day) {
+	        case "monday":
+                System.out.println("lets learn python");
+                break;
+	        case "tuesday":
+                System.out.println("lets learn java");
+                break;
+	        case "wednesday":
+                System.out.println("lets learn C++");
+                break;
+	        case "thursday":
+                System.out.println("lets learn sql");
+                break;
+	        case "friday":
+                System.out.println("lets learn mango db");
+                break;
+	        case "saturday":
+                System.out.println("lets learn html");
+                break;
+	        case "sunday":
+                System.out.println("lets learn css");
+                break;
+            default:
+            	System.out.println("invalid day you entered");
+	        }
+	        
+	        }
+
+}
