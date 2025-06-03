21aec11a97aaaf604272dfbeb5ff395e72c5dff9
Durgadevi-321 <durgakonagantla272@gmail.com>
2025-06-03 15:42:05 +0530

    Add files via upload

---
+++ FavoriteCandy.java
@@ -0,0 +1,18 @@
+package devi;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+
+	public class FavoriteCandy {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			LinkedHashSet<String> candy= new LinkedHashSet<String>();
+			candy.add("chocolate");
+			candy.add("mango");
+			candy.add("watermelon");
+			System.out.println(candy);
+
+		}
+
+	}
+
